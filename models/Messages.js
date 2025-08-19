611efc2cd28727a77c5a7519cbe9ee427726188d
Rajnish Nath <rnish612@gmail.com>
2025-08-19 12:11:13 +0530

    created message schema

---
+++ models/Messages.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: { type: String, required: true },
+    read: { type: Boolean, default: false }, // for read receipts
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
